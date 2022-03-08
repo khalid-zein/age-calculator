946d7abb572f5f70aaadc5a33f0ba09fc2ba79dc
khalid-zein <zeinkhalid004@gmail.com>
2022-03-08 15:58:32 +0300

    added js commands and designed them

--- main.js
+++ main.js
@@ -15,22 +15,28 @@
       var gender = document.querySelector("input[type = 'radio']:checked");
       const name = document.getElementById("name");
       const user = document.getElementById("user");
+      const birth = document.getElementById("birth");
 
       
 
       console.log(date);
       let New = new Date(date);
       let Day =  New.getDay();
+      let Year = New.getFullYear();
     console.log(Day);
     
       if(date === ""){
          return  user.innerHTML = "choose your date of birth!";
-      } else if(gender.value === "female"){
+      }else if(Year > 2022){
+         return birth.innerHTML = "You were not born in future!";
+
+      }else if(gender.value === "female"){
           name.innerHTML ="Your Akan Name is " + femaleAkan[Day];
 
         } else if(gender.value === "male"){
             name.innerHTML = "Your Akan Name is " + maleAkan[Day];
         }
+        birth.innerHTML = "";
         user.innerHTML = "";
         formReset();
 
