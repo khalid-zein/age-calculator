8e1a51e759e8c870f8e9391cfbba5be803c40cd0
khalid-zein <zeinkhalid004@gmail.com>
2022-03-08 14:56:49 +0300

    added inner html

--- main.js
+++ main.js
@@ -5,27 +5,33 @@
   const femaleAkan =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   const btn = document.getElementById("btn");
   
+  function formReset() {
+    document.getElementById("myform").reset();
+  }
   
   btn.addEventListener("click", function (event) {
       event.preventDefault();
-      const date = document.getElementById("date");
-      const gender = document.querySelector("input[type = 'radio']:checked");
+      var date = document.getElementById("date").value;
+      var gender = document.querySelector("input[type = 'radio']:checked");
       const name = document.getElementById("name");
+      const user = document.getElementById("user");
+
       
 
       console.log(date);
-      let userDate = date.value;
-      let New = new Date(userDate);
+      let New = new Date(date);
       let Day =  New.getDay();
-    // console.log(Day);
+    console.log(Day);
+    
+      if(date === ""){
+          user.innerHTML = "choose your date of birth!";
+      } else if(gender.value === "female"){
+          name.innerHTML ="Your Akan Name is " + femaleAkan[Day];
 
-    if(gender.value === "female"){
-        name.innerHTML ="Your Akan Name is " + femaleAkan[Day];
-        // alert(femaleAkan[Day]);
-    }
-    if(gender.value === "male"){
-        name.innerHTML = "Your Akan Name is " + maleAkan[Day];
-        // alert(maleAkan[Day]);
-    }
+        } else if(gender.value === "male"){
+            name.innerHTML = "Your Akan Name is " + maleAkan[Day];
+        }
+
+        formReset();
 
     });
